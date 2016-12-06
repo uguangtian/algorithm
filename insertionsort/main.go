b36552a3701bcfd25f78b406bd6e87b7a2a99ffe
uguangtian <toliuguangtian@gmail.com>
2016-12-06 14:54:07 +0800

    correct insertion sort

--- insertionsort/main.go
+++ insertionsort/main.go
@@ -3,23 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	data := []int{6, 4, 1, 3, 18, 24, 9}
+	data := [7]int{6, 4, 1, 3, 18, 24, 9}
 	insertsort(data)
-
-	for i := 1; i < len(data); i++ {
+	// fmt.Println(data[0])
+	for i := 0; i < len(data); i++ {
 		fmt.Printf("%d ", data[i])
 	}
 }
 
 func insertsort(data []int) {
-	n := data[0]
-	for i, j := 2, 0; i < n+1; i++ {
+	for i, j := 1, 0; i < len(data); i++ {
 		j = i - 1
 		temp := data[i]
-		for j > 0 && temp < data[j] {
+		// if need swap
+		for j >= 0 && temp < data[j] {
 			data[j+1] = data[j]
 			j--
 		}
+
+		//Just hold on if havn't swap
 		data[j+1] = temp
 	}
 }
